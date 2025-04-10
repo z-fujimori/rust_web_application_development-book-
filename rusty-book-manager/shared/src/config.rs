d8077ba51c924442da3234a2e0b6f7b6c27a7309
Taichi Fujimori <fuji.tai.29@i.softbank.jp>
2025-04-10 22:07:53 +0900

    ~82 (4章)

---
+++ rusty-book-manager/shared/src/config.rs
@@ -0,0 +1,27 @@
+use anyhow::Result;
+
+pub struct AppConfig {
+    pub database: DatabaseConfig,
+}
+
+impl AppConfig {
+    // DB接続に必要な情報を環境変数から取り出す
+    pub fn new() -> Result<Self> {
+        let database = DatabaseConfig {
+            host: std::env::var("DATABASE_HOST")?,
+            port: std::env::var("DATABASE_PORT")?.parse()?,
+            username: std::env::var("DATABASE_USERNAME")?,
+            password: std::env::var("DATABASE_PASSWORD")?,
+            database: std::env::var("DATABASE_NAME")?
+        };
+        Ok(Self { database })
+    }
+}
+
+pub struct DatabaseConfig {
+    pub host: String,
+    pub port: u16,
+    pub username: String,
+    pub password: String,
+    pub database: String
+}
